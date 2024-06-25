5138fccd4c2c440f7a62308ca8b4c7d4ab025cda
JBUkim <91812103@jmail.ac.kr>
2024-06-25 12:42:48 +0900

    css_change

--- src/main/java/dev/mvc/board/BoardProc.java
+++ src/main/java/dev/mvc/board/BoardProc.java
@@ -116,33 +116,9 @@ public class BoardProc implements BoardProcInter {
     StringBuffer str = new StringBuffer(); // String class 보다 문자열 추가등의 편집시 속도가 빠름
 
     // style이 java 파일에 명시되는 경우는 로직에 따라 css가 영향을 많이 받는 경우에 사용하는 방법
-    str.append("<style type='text/css'>");
-    str.append("  #paging {text-align: center; margin-top: 5px; font-size: 1em;}");
-    str.append("  #paging A:link {text-decoration:none; color:black; font-size: 1em;}");
-    str.append("  #paging A:hover{text-decoration:none; background-color: #FFFFFF; color:black; font-size: 1em;}");
-    str.append("  #paging A:visited {text-decoration:none;color:black; font-size: 1em;}");
-    str.append("  .span_box_1{");
-    str.append("    text-align: center;");
-    str.append("    font-size: 1em;");
-    str.append("    border: 1px;");
-    str.append("    border-style: solid;");
-    str.append("    border-color: #cccccc;");
-    str.append("    padding:1px 6px 1px 6px; /*위, 오른쪽, 아래, 왼쪽*/");
-    str.append("    margin:1px 2px 1px 2px; /*위, 오른쪽, 아래, 왼쪽*/");
-    str.append("  }");
-    str.append("  .span_box_2{");
-    str.append("    text-align: center;");
-    str.append("    background-color: #668db4;");
-    str.append("    color: #FFFFFF;");
-    str.append("    font-size: 1em;");
-    str.append("    border: 1px;");
-    str.append("    border-style: solid;");
-    str.append("    border-color: #cccccc;");
-    str.append("    padding:1px 6px 1px 6px; /*위, 오른쪽, 아래, 왼쪽*/");
-    str.append("    margin:1px 2px 1px 2px; /*위, 오른쪽, 아래, 왼쪽*/");
-    str.append("  }");
-    str.append("</style>");
-    str.append("<DIV id='paging'>");
+    str.append(" <div class='col-12'>");
+    str.append(" <div class='pagination d-flex justify-content-center mt-5'>");
+
 //    str.append("현재 페이지: " + nowPage + " / " + totalPage + "  "); 
 
     // 이전 10개 페이지로 이동
@@ -153,7 +129,7 @@ public class BoardProc implements BoardProcInter {
     // 현재 3그룹일 경우: (3 - 1) * 10 = 2그룹의 마지막 페이지 20
     int _now_page = (now_grp - 1) * page_per_block;
     if (now_grp >= 2) { // 현재 그룹번호가 2이상이면 페이지수가 11페이지 이상임으로 이전 그룹으로 갈수 있는 링크 생성
-      str.append("<span class='span_box_1'><A href='" + list_file + "?crudcateno="+crudcateno+"&word=" + word + "&now_page=" + _now_page
+      str.append("<span><A class='rounded' href='" + list_file + "?crudcateno="+crudcateno+"&word=" + word + "&now_page=" + _now_page
           + "'>이전</A></span>");
     }
 
@@ -164,10 +140,10 @@ public class BoardProc implements BoardProcInter {
       }
 
       if (now_page == i) { // 목록에 출력하는 페이지가 현재페이지와 같다면 CSS 강조(차별을 둠)
-        str.append("<span class='span_box_2'>" + i + "</span>"); // 현재 페이지, 강조
+        str.append("<span><A class='active rounded'>" + i + "</A></span>"); // 현재 페이지, 강조
       } else {
         // 현재 페이지가 아닌 페이지는 이동이 가능하도록 링크를 설정
-        str.append("<span class='span_box_1'><A href='" + list_file + "?crudcateno="+crudcateno +"&word=" + word + "&now_page=" + i + "'>" + i
+        str.append("<span><A class='rounded' href='" + list_file + "?crudcateno="+crudcateno +"&word=" + word + "&now_page=" + i + "'>" + i
             + "</A></span>");
       }
     }
@@ -179,10 +155,12 @@ public class BoardProc implements BoardProcInter {
     // 현재 페이지 25일경우 -> 현재 3그룹: (3 * 10) + 1 = 4그룹의 시작페이지 31
     _now_page = (now_grp * page_per_block) + 1; // 최대 페이지수 + 1
     if (now_grp < total_grp) {
-      str.append("<span class='span_box_1'><A href='" + list_file + "?crudcateno="+crudcateno +"&word=" + word + "&now_page=" + _now_page
+      str.append("<span><A class='rounded' href='" + list_file + "?crudcateno="+crudcateno +"&word=" + word + "&now_page=" + _now_page
           + "'>다음</A></span>");
     }
     str.append("</DIV>");
+    str.append("</DIV>");
+    
 
     return str.toString();
   }
@@ -209,7 +187,7 @@ public class BoardProc implements BoardProcInter {
     // 3 group: 21, 22 ... 29, 30
     int start_page = ((now_grp - 1) * page_per_block) + 1; // 특정 그룹의 시작 페이지  
     int end_page = (now_grp * page_per_block);               // 특정 그룹의 마지막 페이지   
-     
+    
     StringBuffer str = new StringBuffer(); // String class 보다 문자열 추가등의 편집시 속도가 빠름 
     
     // style이 java 파일에 명시되는 경우는 로직에 따라 css가 영향을 많이 받는 경우에 사용하는 방법
