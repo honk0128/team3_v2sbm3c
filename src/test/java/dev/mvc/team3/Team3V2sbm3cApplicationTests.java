d607b10bfa496e08a78c404c8550d6d2b475f9fd
JBUkim <91812103@jmail.ac.kr>
2024-07-18 11:06:09 +0900

    test 주석처리

--- src/test/java/dev/mvc/team3/Team3V2sbm3cApplicationTests.java
+++ src/test/java/dev/mvc/team3/Team3V2sbm3cApplicationTests.java
@@ -1,13 +1,13 @@
-package dev.mvc.team3;
+// package dev.mvc.team3;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Team3V2sbm3cApplicationTests {
+// @SpringBootTest
+// class Team3V2sbm3cApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
